707bc4a1f07b8a99e3d26561b37749afc7541952
Konstantin Gladkiy <666xhot666@gmail.com>
2021-12-21 15:44:13 +0200

    Prepare to setup OCAPI

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -12,33 +12,27 @@ $(document).ready(() => {
   $('.goTop').click(() => {
     scroll(0, 0)
   })
-  $('.downloadCSV').click((e) => {
-    e.preventDefault()
-    window.location.href = '/api/users.csv'
-  })
 
-  $.get('/api/users', (data) => {
-    const { users } = data
-    if (users) {
-      users.map((user) => {
+  $.get('/api/user?credentials=dGVzdEBnbWFpbC5jb206MHlXKkooaiV6TA==', (data) => {
+    const { customer } = data
+    if (customer) {
         let card = $('<div></div>').addClass('card-item')
-        let cardAvatar = $('<div></div>').addClass('card-avatar left')
-        let img = $('<img>').attr({ src: `${user.avatar}` })
+        // let cardAvatar = $('<div></div>').addClass('card-avatar left')
+        // let img = $('<img>').attr({ src: `${user.avatar}` })
         let cardContent = $('<div></div>').addClass('card-content')
         let contentTitle = $('<div></div>')
           .addClass('title')
-          .text(`${user.first_name} ${user.last_name}`)
-        let email = $('<div></div>').addClass('email').text(`${user.email}`)
+          .text(`${customer.first_name} ${customer.last_name}`)
+        let email = $('<div></div>').addClass('email').text(`${customer.email}`)
+        let loyalty = $('<div></div>').addClass('email').text(`isLoyalty: ${customer.c_isLoyalty}`)
         $('.cards-list').append(
           card.append(
-            cardAvatar.append(img),
             cardContent.append(contentTitle),
-            email
+            email,
+            loyalty
           )
         )
-      })
-
-      $('.card-action').css({ opacity: '1' })
+      // $('.card-action').css({ opacity: '1' })
     } else {
       const errorMessage = $('<div></div>').addClass('errorMessage')
       const text = $('<h4></h4>').text(
@@ -46,48 +40,10 @@ $(document).ready(() => {
       )
       $('.cards-list').append(errorMessage.append(text))
     }
-  }).fail(() => {
+  }).fail((error) => {
     const errorMessage = $('<div></div>').addClass('errorMessage')
     const text1 = $('<h4></h4>').text('Oops, something went wrong!!!')
-    const text2 = $('<h4></h4>').text(
-      'Can`t load Users. Please reload the page, or try again later'
-    )
+    const text2 = $('<h4></h4>').text(error.responseJSON.message)
     $('.cards-list').append(errorMessage.append(text1, text2))
-  })
-  $('#load-more').click(() => {
-    page += 1
-    $.get(`/api/users?page=${page}`, (data) => {
-      const { users } = data
-      if (!users) {
-        let span = $('<p></p>').text('All users Loaded')
-        $('#load-more').remove()
-        $('.card-action').append(span)
-      } else {
-        users.map((user) => {
-          let card = $('<div></div>').addClass('card-item')
-          let cardAvatar = $('<div></div>').addClass('card-avatar left')
-          let img = $('<img>').attr({ src: `${user.avatar}` })
-          let cardContent = $('<div></div>').addClass('card-content')
-          let contentTitle = $('<div></div>')
-            .addClass('title')
-            .text(`${user.first_name} ${user.last_name}`)
-          let email = $('<div></div>').addClass('email').text(`${user.email}`)
-          $('.cards-list').append(
-            card.append(
-              cardAvatar.append(img),
-              cardContent.append(contentTitle),
-              email
-            )
-          )
-        })
-      }
-    }).fail(() => {
-      const errorMessage = $('<div></div>').addClass('errorMessage')
-      const text1 = $('<h4></h4>').text('Ooops, something went wrong!!!')
-      const text2 = $('<h4></h4>').text(
-        'Can`t load Users. Please reload the page, or try again later'
-      )
-      $('.cards-list').append(errorMessage.append(text1, text2))
-    })
   })
 })
--- src/routes/usersRouter.js
+++
@@ -1,5 +0,0 @@
-const { Router } = require('express')
-const userController = require('../controllers/usersController.js')
-const usersRouter = Router()
-usersRouter.get('/', userController)
-module.exports = usersRouter
